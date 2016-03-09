c66e49ec76daa65e1fb55f0a8a950a6edf4ce1ea
sach <sacha.saint-leger@mail.mcgill.ca>
2016-03-09 18:12:16 -0500

    river and lake data along with supplemental river data for north america merged into one topojson file and displaying successfully in browser

--- river.js
+++ river.js
@@ -14,18 +14,44 @@ function northAmericanRivers() {
     var path = d3.geo.path()
         .projection(projection);
     
-    d3.json("data/ne_10m_rivers_north_america/narstopo.json", function(error, rivers) {
+//    d3.json("data/ne_10m_rivers_north_america/narstopo.json", function(error, rivers) {
+//	if (error) return console.error(error);
+//	console.log(rivers.objects.supplement.geometries);
+
+//	svg.insert("path", ".graticule")
+//	    .datum(topojson.feature(rivers, rivers.objects.supplement))
+    //	    .attr("d", path);
+
+    //});
+
+  
+ //   d3.json("data/ne_10m_rivers_lake_centerlines/rivers.json", function(error, rivers) {
+//	if (error) return console.error(error);
+//	console.log(rivers.objects.rivers.geometries);
+
+//	svg.insert("path", ".graticule")
+//	    .datum(topojson.feature(rivers, rivers.objects.rivers))
+  //  	    .attr("d", path)
+//	    .attr("fill", blue);
+  //  });
+
+    d3.json("data/all-rivers-lakes-north-america.json", function(error, rivers) {
 	if (error) return console.error(error);
-	console.log(rivers.objects.supplement.geometries);
+	console.log(rivers.objects);
 
 	svg.insert("path", ".graticule")
-	    .datum(topojson.feature(rivers, rivers.objects.supplement))
-	    .attr("d", path);
-	
-		   
-	
-    });
+	    .datum(topojson.feature(rivers, rivers.objects.riversandlakes))
+    	    .attr("d", path)
+	    .style("fill", "blue")
 
+	svg.insert("path", ".graticule")
+	    .datum(topojson.feature(rivers, rivers.objects.supplemental))
+    	    .attr("d", path)
+	    .style("fill", "blue")
+
+      });
+
+  
 
 
 

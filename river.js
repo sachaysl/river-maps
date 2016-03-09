86a5bee310910f1baca4eed565bd697f5bfe8f9c
sach <sacha.saint-leger@mail.mcgill.ca>
2016-03-09 17:44:33 -0500

    supplement rivers displaying as required

--- river.js
+++ river.js
@@ -1,8 +1,29 @@
 function northAmericanRivers() {
 
+    var width = 960,
+	height = 500;
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var projection = d3.geo.albersUsa()
+        .scale(1000)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geo.path()
+        .projection(projection);
+    
     d3.json("data/ne_10m_rivers_north_america/narstopo.json", function(error, rivers) {
 	if (error) return console.error(error);
-	console.log(rivers);
+	console.log(rivers.objects.supplement.geometries);
+
+	svg.insert("path", ".graticule")
+	    .datum(topojson.feature(rivers, rivers.objects.supplement))
+	    .attr("d", path);
+	
+		   
+	
     });
 
 
